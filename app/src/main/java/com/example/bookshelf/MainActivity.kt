4c5b47f6d275c87c3e1764641b03c7cfcd1177c2
myakisheff <n.s.myakishev@yandex.ru>
2024-09-16 15:41:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bookshelf/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.bookshelf
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.bookshelf.ui.theme.BookshelfTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BookshelfTheme {
+
+            }
+        }
+    }
+}
